a947d846145bd84b8168752ea533c05321335261
TM02 <77006076+TM02@users.noreply.github.com>
2023-12-19 11:44:25 +0800

    Add files via upload

---
+++ aws-live-main/config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cjz0lxm4rce0.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "ongtzimin-employee"
+customregion = "us-east-1"
+
